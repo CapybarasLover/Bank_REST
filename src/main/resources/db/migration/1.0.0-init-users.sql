9d3bead4b856f6c8384af89defbb3b9a26344a5e
CapibaraLover <merzlikin.0404@mail.ru>
2026-03-04 23:11:45 +0300

    ADD закончил инит ченжсет таблицы users

--- src/main/resources/db/migration/1.0.0-init-users.sql
+++ src/main/resources/db/migration/1.0.0-init-users.sql
@@ -1,8 +1,13 @@
 --liquibase formatted sql
---changeset petrm:1.0.0-init
+--changeset petrm:1.0.0-users
 
 -- TODO доделать чейнжсеты (добавить ост поля и еще таблицы)
 CREATE TABLE users (
-    id          BIGSERIAL       PRIMARY KEY,
-    username    VARCHAR(255)    NOT NULL    UNIQUE,
-)
+    id          BIGINT          GENERATED ALWAYS AS IDENTITY        PRIMARY KEY,
+    username    VARCHAR(255)    NOT NULL                            UNIQUE,
+    password    VARCHAR(255)    NOT NULL,
+    role        VARCHAR(50)     NOT NULL                            DEFAULT 'USER',
+    enabled     BOOLEAN         NOT NULL                            DEFAULT TRUE
+);
+
+--rollback DROP TABLE users;
---
+++ src/main/resources/db/migration/1.0.1-init-cards.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+--changeset petrm:1.0.1-cards
+
+CREATE TABLE cards(
+
+);
+
+--rollback DROP TABLE cards;
