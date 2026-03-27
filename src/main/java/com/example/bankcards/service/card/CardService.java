5a5313527bb5737b6301fc7c2fa720539a98f59c
CapibaraLover <merzlikin.0404@mail.ru>
2026-03-27 17:13:32 +0300

    ADD начал описывать CardService

--- src/main/java/com/example/bankcards/service/card/CardService.java
+++ src/main/java/com/example/bankcards/service/card/CardService.java
@@ -1,5 +1,14 @@
 package com.example.bankcards.service.card;
 
+import com.example.bankcards.dto.CardDto;
+import com.example.bankcards.dto.UserDto;
+
+import java.util.List;
+
 public interface CardService {
 // TODO описать методы сервиса карт
+    List<CardDto> getUserCards(UserDto userDto);
+    String createCard(UserDto userDto);
+    String deleteCard(String cardholder, String cardNum);
+    String deleteCardRequest(UserDto userDto, String cardNum);
 }
